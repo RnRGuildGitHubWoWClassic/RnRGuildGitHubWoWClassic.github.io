3bcdd70daf9b054b7feac1019ad3693402180117
Michorov <michorov@outlook.com>
2019-12-11 11:46:05 +0200

    Added sorting by name

--- script.js
+++ script.js
@@ -1,29 +1,55 @@
 let upwards = 0;
 
-window.onload = function() {
-  let dkp = document.getElementsByClassName("divDKPHeader")[0];
-  dkp.innerHTML = '<a href="">Dkp</a>';
+function addSorting(buttonClass, buttonName) {
+  let dkp = document.getElementsByClassName(buttonClass)[0];
+  dkp.innerHTML = `<a href="">${buttonName}</a>`;
 
   dkp.addEventListener("click", function(event) {
     event.preventDefault();
     let list = document.getElementsByClassName("divTableRow");
     let sortedList = Array.prototype.slice.call(list);
 
+    let header;
+
     if (upwards == 0) {
-      sortedList.sort((a, b) => {
-        return (
-          +b.getElementsByTagName("div")[2].innerHTML -
-          +a.getElementsByTagName("div")[2].innerHTML
-        );
-      });
+      if (buttonClass == "divDKPHeader") {
+        header = sortedList[0];
+        sortedList.sort((a, b) => {
+          return (
+            +b.getElementsByTagName("div")[2].innerHTML -
+            +a.getElementsByTagName("div")[2].innerHTML
+          );
+        });
+      } else {
+        header = sortedList[0];
+        sortedList.sort((a, b) => {
+          return b
+            .getElementsByTagName("div")[0]
+            .textContent.localeCompare(
+              a.getElementsByTagName("div")[0].textContent
+            );
+        });
+      }
       upwards = 1;
     } else {
-      sortedList.sort((a, b) => {
-        return (
-          +a.getElementsByTagName("div")[2].innerHTML -
-          +b.getElementsByTagName("div")[2].innerHTML
-        );
-      });
+      if (buttonClass == "divDKPHeader") {
+        header = sortedList[0];
+        sortedList.sort((a, b) => {
+          return (
+            +a.getElementsByTagName("div")[2].textContent -
+            +b.getElementsByTagName("div")[2].textContent
+          );
+        });
+      } else {
+        header = sortedList[0];
+        sortedList.sort((a, b) => {
+          return a
+            .getElementsByTagName("div")[0]
+            .textContent.localeCompare(
+              b.getElementsByTagName("div")[0].textContent
+            );
+        });
+      }
       upwards = 0;
     }
 
@@ -32,5 +58,11 @@ window.onload = function() {
     for (let item of sortedList) {
       table.appendChild(item);
     }
+    table.prepend(header);
   });
+}
+
+window.onload = function() {
+  addSorting("divDKPHeader", "DKP");
+  addSorting("divPlayerHeader", "Player");
 };
